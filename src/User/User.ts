9f90f42420c8d4a360a6f80e94a1fd9776cb57a7
Tanner Goins <tannergz28@gmail.com>
2018-02-03 00:06:30 -0600

    add lastMessage column

--- src/User/User.ts
+++ src/User/User.ts
@@ -48,6 +48,9 @@ export class User {
   @SchemaField(Array)
   favorites: []
 
+  @Column()
+  lastMessage: Date
+
   @OneToOne(type => UserBalance, userBalance => userBalance.user, {
     cascadeAll: true
   })
